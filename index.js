7fb8096f6cefdbf92349b633e64c580fbf3da488
TNLISN <nicolas45lav@gmail.com>
2022-10-20 15:17:22 +0200

    1

---
+++ index.js
@@ -0,0 +1,21 @@
+// We can use express as shown as below
+const express = require('express')
+const app = express()
+var cars = require("./vehicles");
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/vehicles', (req, res) => {
+    res.json({cars})
+})
+app.get('/vehicles/([a-zA-Z]+)', (req, res) => {   
+    const origin = req.url.split("/")[2];
+    const car = cars.filter((car) => car.Origin === origin);
+    res.json(car)
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
